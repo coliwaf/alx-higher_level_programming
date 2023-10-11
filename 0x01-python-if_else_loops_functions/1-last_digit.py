3008c89c0ed7bafd5ae97fcabd6f43a735bc2be0
Collins Sande <coliwafula@gmail.com>
2023-10-11 17:52:14 +0300

    print alphabet

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -9,10 +9,9 @@ if number < 0:
 output =  'Last digit of {} and is {} and is '.format(number, last_digit);
 if last_digit > 5:
     output += 'greater than 5'
-    print(output)
 elif last_digit == 0:
     output += '0'
-    print(output)
 else:
     output += 'than 6 and not 0'
-    print(output)
+
+print(output)
