7b650c27810eb0f02f890bb57400260f7b1f510b
Collins Sande <coliwafula@gmail.com>
2023-10-24 21:03:01 +0300

    my calc

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+
+if __name__ == "__main__":
+    import sys
+    from calculator_1 import add, sub, mul, div
+
+    argc = len(sys.argv)
+    funcs = {
+            '+': add,
+            '-': sub,
+            '*': mul,
+            '/': div
+            }
+
+    if argc != 4:
+        print ('Usage: {} <a> <operator> <b>'.format(sys.argv[0]))
+        sys.exit(1)
+
+    if sys.argv[2] not in list(funcs.keys()):
+        print('Unknown operator. Available operators: +, -, * and /')
+        sys.exit(1)
+
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    
+    func = funcs[sys.argv[2]]
+    res = func(a, b)
+
+    print('{:d} {:s} {:d} = {:d}'.format(a, sys.argv[2], b, res))
