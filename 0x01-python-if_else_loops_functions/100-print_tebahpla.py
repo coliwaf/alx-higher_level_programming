9f036db569159eb409a28247424955b63f767a40
Collins Sande <coliwafula@gmail.com>
2023-10-24 19:42:36 +0300

    print tebahpla

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+
+i = 122
+while i >96:
+    c = 0
+    if i % 2 != 0:
+        i -= 32
+        c = 1
+
+    print("{}".format(chr(i)), end="")
+
+    if c == 1:
+        i += 32
+
+    i -= 1
