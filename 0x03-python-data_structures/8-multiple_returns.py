95920705d9221b2d82133cbe9eaf81d32154e25d
Collins Sande <coliwafula@gmail.com>
2023-10-24 04:19:48 +0300

    multiple returns

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def multiple_returns(sentence):
+
+    length = len(sentence)
+
+    if (length < 1):
+        return (0, None)
+
+    first_char = sentence[0]
+    tup_res = length, first_char
+    return(tup_res)
