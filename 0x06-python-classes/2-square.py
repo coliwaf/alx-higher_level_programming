253ddc364be758d13bc9299153e6d04646a0e543
Collins Sande <coliwafula@gmail.com>
2023-10-25 03:17:30 +0300

    size validation with docs

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
+"""A square module"""
 
 
 class Square:
+    """Define a square"""
 
-    def __init__(self, size=0):
-
+    def __init__(self, size):
+        """Initialize the new square
+        Args:
+            size: size of the new square
+        """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
