48bcb8b4e4a47f973eb0ba835c7e98de26c32918
Collins Sande <coliwafula@gmail.com>
2023-10-25 03:12:40 +0300

    adding documentation

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
+"""A square module"""
 
 
 class Square:
+    """Define a square"""
 
     def __init__(self, size):
+        """Initialize the new square
+        Args:
+            size: size of the new square
+        """
         self.__size = size
