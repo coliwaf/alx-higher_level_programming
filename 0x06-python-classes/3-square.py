e57d8d165e9b7247cb9bef5222941bbd7ea8e7be
Collins Sande <coliwafula@gmail.com>
2023-10-25 03:21:04 +0300

    three square

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,15 +1,21 @@
 #!/usr/bin/python3
+"""A square module"""
 
 
 class Square:
+    """Define a square"""
 
-    def __init__(self, size=0):
-
+    def __init__(self, size):
+        """Initialize the new square
+        Args:
+            size: size of the new square
+        """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
 
-    def area(self):
-        return (self.__size * self.__size)
+        def area(self):
+            """Returns current area of the square"""
+            return (self.__size * self.__size)
