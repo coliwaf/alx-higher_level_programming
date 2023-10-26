83656454d64bd1ccbd627aa3b759e4e443fe29ff
Collins Sande <coliwafula@gmail.com>
2023-10-26 05:08:20 +0300

    5 square

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""A square module"""
+
+
+class Square:
+    """Define a square"""
+
+    def __init__(self, size=0):
+        """Initialize the new square
+        Args:
+            size: size of the new square
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """ Size of the square lengthwise
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        Raises:
+            TypeError: raised if size is not an integer
+            ValueError: raised if size is less than 0
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Area of this square
+
+        Returns: size of the square
+        """
+        return self.__size ** 2
+
+    def my_print(self):
+        """Public instace that prints this square
+        using the # char
+        """
+
+        for i in range(self.size):
+            for j in range(self.size):
+                print("#", end="")
+            print("")
+
+        if self.size == 0:
+            print("")
