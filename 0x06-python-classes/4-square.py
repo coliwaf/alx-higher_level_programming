70627738f3396751c4e80cde33dad9745d4f0bca
Collins Sande <coliwafula@gmail.com>
2023-10-26 04:57:36 +0300

    4 square corrections

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -9,14 +9,7 @@ class Square:
         """Initialize the new square
         Args:
             size: size of the new square
-        Raises:
-            TypeError: raised if size is not an int
-            ValueError: raised if size is less than 0
         """
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
         self.__size = size
 
     @property
