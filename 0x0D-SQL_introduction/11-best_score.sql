44e3538fe12177354975269464b63c3171c0a179
Collins Sande <coliwafula@gmail.com>
2023-12-28 14:30:43 +0300

    best score

---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,3 @@
+-- Lists all records with a score equalorgreater than 10
+-- while showing name and score only, ordered by score
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
