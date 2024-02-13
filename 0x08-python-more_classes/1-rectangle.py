7d2cdaa105548eae3ded65ba75ce1995d2967e20
Collins Sande <coliwafula@gmail.com>
2024-02-13 05:35:43 +0300

    1-rectangle

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -27,7 +27,7 @@ class Rectangle:
             TypeError: if width is not integer
             ValueError: if width is less than zero
         """
-        if not instance(value, int):
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
@@ -46,7 +46,7 @@ class Rectangle:
             TypeError: if height is not integer
             ValueError: if height is less than zero
         """
-        if not instance(value, int):
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
