592b2174bb2c0adbf0e331398ae8da962c941d39
Collins Sande <coliwafula@gmail.com>
2024-02-21 00:58:57 +0300

    Json string to object

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Module defines a json-to-object method
+"""
+import json
+
+
+def from_json_string(my_str)):
+    """
+    Returns a Python Obj of a Json string
+    """
+    return (json.loads(my_str))
