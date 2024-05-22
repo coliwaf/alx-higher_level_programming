60f409c3188810a16c9e7fcdcb7fbaf3ac20ef51
Collins Sande <coliwafula@gmail.com>
2024-05-22 04:36:21 +0300

    list all and order by score

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,3 @@
+-- Lists all records of "second_table", displaying columns in order of 
+-- 'score' and 'name' and the results ordered by 'score'(top first)
+SELECT `score`, `name` FROM `second_table` ORDER BY score DESC;
