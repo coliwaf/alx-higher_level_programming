406dec653548dc7d6bf7544c18a393823426a2dc
Collins Sande <coliwafula@gmail.com>
2024-05-22 05:09:14 +0300

    record grouping

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- Lists the number of records with the same score in the table 'second_table'
-SELECT `score`, COUNT(`score`) AS number FROM `second_table` GROUP_BY `score` ORDER_BY `score` DESC;
+SELECT `score`, COUNT(`score`) AS `number` FROM `second_table` GROUP BY `score` ORDER BY `score` DESC;
