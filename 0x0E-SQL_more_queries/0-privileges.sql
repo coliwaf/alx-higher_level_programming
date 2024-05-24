31f152ba9bc584a24094b9670bb57122a386ae68
Collins Sande <coliwafula@gmail.com>
2024-05-24 04:04:14 +0300

    privileges

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,5 +1,5 @@
 -- Lists all user_0d_1 and user_0d_2 privileges in localhost
 -- in the MySQL server
-SHOW GRANTS FOR 'user_0d_1'@'localhost'
-SHOW GRANTS FOR 'user_0d_2'@'localhost'
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
 
