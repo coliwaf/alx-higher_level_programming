3842572ab66c15450c22b4f722eac3de33f57b6f
Collins Sande <coliwafula@gmail.com>
2024-06-18 01:37:30 +0300

    an integer

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const num = process.argv[2];
+if (isNaN(num) || num === undefined) {
+	console.log('Not a number');
+} else {
+	const numInt = parseInt(num);
+	console.log(`My number: ${numInt}`);
+}
